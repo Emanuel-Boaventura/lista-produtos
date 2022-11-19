6b67214cb4cf8a1bedba715dffb3c47f2a33b34f
Emanuel-Boaventura <emanuelbmatos321@gmail.com>
2022-11-18 22:04:35 -0400

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api';
+import { useState, useEffect } from 'react';
+import Loading from './pages/Loading';
 
 function App() {
+  const [data, setData] = useState('');
+
+  async function getData() {
+    try {
+      const { data } = await api.get('/produtos');
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (!data) return <Loading />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Produtos</h1>
+
+        <button>Adicionar Produto</button>
       </header>
+
+      <div className='list'>
+        <div className='head-list container'>
+          <span className='name'>Nome</span>
+          <span className='category'>Categorias</span>
+          <span className='price'>Preço</span>
+          <span className='date'>Data de Criação</span>
+          <span className='actions'>Ações</span>
+        </div>
+
+        {data.map((itens) => (
+          <div className='products container' key={itens.id}>
+            <span className='name'>{itens.name}</span>
+            <span className='category'>{itens.category}</span>
+            <span className='price'>{itens.price}</span>
+            <span className='date'>{itens.date}</span>
+            <span className='actions'>Editar / Deletar</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/pages/Loading/index.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className='App'>
+      <h1>Carregando...</h1>
+      <p>Aguarde um momento.</p>
+    </div>
+  );
+};
+
+export default Loading;
